34a6810b4f17e47cc451545b506cf19273cd722e
ramalakshmiNet <53755339+ramalakshmiNet@users.noreply.github.com>
2019-08-19 20:51:29 +0530

    Add files via upload

--- CoreMVC/RamasPieShop/RamasPieShop/Controllers/HomeController.cs
+++ CoreMVC/RamasPieShop/RamasPieShop/Controllers/HomeController.cs
@@ -27,5 +27,13 @@ namespace RamasPieShop.Controllers
             };
             return View(homeViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
